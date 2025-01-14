e1a93e3c8efe8ea447597395a0f16ddc2f5863eb
dorsaslm <134289196+dorsaslm@users.noreply.github.com>
2025-01-14 19:47:46 -0400

    Time: 3 ms (49.94%) | Memory: 19 MB (25.62%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -7,15 +7,21 @@ class Solution:
         previous=nums[0]
         underlines=0
         i = 1
+        k = 1
+
+        for i in range (1,len(nums)):
+            if nums[i] != nums[k - 1]:  # Compare with the last unique element
+                nums[k] = nums[i]  # Place the unique element at the `k` index
+                k += 1
     
-        while i < len(nums):
-            if nums[i] == previous:
-                nums.pop(i)
-            else:
-                previous = nums[i]
-                i += 1
+        #while i < len(nums):
+        #    if nums[i] == previous:
+        #        nums.pop(i)
+        #    else:
+        #        previous = nums[i]
+        #        i += 1
         
-        return len(nums)
+        return k
 
 
 
