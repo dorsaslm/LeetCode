9075d7f7057b1ec405593c2a703dc73fdb9ec80d
dorsaslm <134289196+dorsaslm@users.noreply.github.com>
2025-11-08 13:08:40 -0400

    Time: 380 ms (38.09%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
-select name as Employee 
+/*select name as Employee 
 from Employee E1
 where E1.managerid is not null and 
-(select E2.salary from Employee E2 where E2.id = E1.managerId) < E1.salary
+(select E2.salary from Employee E2 where E2.id = E1.managerId) < E1.salary*/
+
+
+select E1.name as Employee
+from Employee E1
+inner join Employee E2
+on E1.managerid = E2.id
+where E1.salary > E2.salary
